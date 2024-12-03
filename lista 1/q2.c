f9c6a35702117b8b20d477b7100d785121e2f02a
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-12-03 14:00:47 -0300

    questao 2

---
+++ lista 1/q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float num;
+    
+    printf("Digite um número do tipo float:\n");
+    scanf("%f", &num);
+
+    printf("E o seu número com 1 ponto flutuante é: %.1f", num);
+
+    return 0;
+}
