3339446322ff1dd03c62ae9c222a2797e0af6858
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-12-03 14:18:15 -0300

    questao 7

---
+++ lista 1/q7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Digite um valor inteiro:\n");
+    scanf("%d", &num);
+
+    printf("O valor ao quadrado é: %d", num*num);
+
+    return 0;
+};
