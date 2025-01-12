1720541e81e243a91e82c69755178ead5011ce23
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2025-01-12 14:28:39 -0300

    questao 1

--- lista 5/q1.c
+++ lista 5/q1.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int num1, num2, soma;
-    int *ptr1, *ptr2, *ptrSoma;
+int main(){
+    int x, y, r;
 
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
-    
-    printf("Digite o segundo número: ");
-    scanf("%d", &num2);
+    int *px, *py, *pr;
 
-    ptr1 = &num1;
-    ptr2 = &num2;
-    ptrSoma = &soma;
+    px = &x; 
+    py = &y;
+    pr = &r;
 
-    *ptrSoma = *ptr1 + *ptr2;
+    puts("Digite o primeiro valor: ");
+    scanf("%d",px);
 
-    printf("A soma é: %d\n", *ptrSoma);
-    printf("O endereço de memória da soma é: %p\n", (void*)ptrSoma);
+    puts("Digite o segundo valor: ");
+    scanf("%d",py);
+
+
+    *pr = *px + *py;
+
+    printf("[%p] %d + [%p] %d = [%p] %d\n\n",px, *px, py, *py, pr, *pr );
+
 
     return 0;
 }
